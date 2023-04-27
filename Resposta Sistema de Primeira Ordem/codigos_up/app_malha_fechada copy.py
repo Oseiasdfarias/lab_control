9272a95fb86bfcd5b30a25cf49e5b1dac7aa91ae
Oseiasdfarias <oseias.dfarias@gmail.com>
2023-04-27 18:59:25 -0300

    teste

--- Resposta Sistema de Primeira Ordem/codigos_up/app_malha_fechada copy.py
+++ Resposta Sistema de Primeira Ordem/codigos_up/app_malha_fechada copy.py
@@ -16,22 +16,25 @@ from scipy.signal import square, sawtooth
 
 ##########################################
 
-numAmostras = 400
+numAmostras = 300
 tempo = np.zeros(numAmostras)
 y = np.zeros(numAmostras)
 
 Ts = 0.02
 
 fre = 0.5
-Amplitude = 1.0
-setpoint = 6.5
+Amplitude = 0.5
+setpoint = 6
 # a = 2*np.ones(int(numAmostras/2))
-# b = 4*np.ones(int(numAmostras/2))/dev/ttyACM0 
+# b = 4*np.ones(int(numAmostras/2))
 # u = np.concatenate([a,b]) #degrau
 r = np.zeros(numAmostras)
 toc = np.zeros(numAmostras)
+erro = np.zeros(numAmostras)
+u = np.zeros(numAmostras)
 ######################
-
+Kp = 2.5
+# Ki = 1
 
 for n in range(numAmostras):
     r[n] = Amplitude*square(2*np.pi*fre*n*Ts) + setpoint
@@ -40,7 +43,6 @@ for n in range(numAmostras):
     # r[n] = u[n]
 
 print('\nEstabelecendo conexão.')
-# conexao = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=0.005)
 conexao = serial.Serial(port='COM4', baudrate=9600, timeout=0.005)
 
 t.sleep(1)
@@ -53,8 +55,17 @@ for n in range(numAmostras):
 
         y[n] = conexao.readline().decode()
 
-    u = (r[n]*255)/15
-    conexao.write(str(round(u)).encode())
+    erro[n] = r[n] - (y[n] - 4.56)
+    #print(y[n])
+    u[n] = (Kp*erro[n]) + 7  # u[n-1] + 2.5*erro[n] - 1.5*erro[n-1] + 5.5  # Controlador Proporcional Integral
+    print(u[n])
+    if (u[n] >= 7):
+        sinal_enviado = 255
+
+    else:
+        sinal_enviado = (u[n]*255)/7
+
+    conexao.write(str(round(sinal_enviado)).encode())
 
     t.sleep(Ts)
 
@@ -71,16 +82,16 @@ print('Nivel_DC:', np.mean(y[tempo > 2]))
 
 plt.figure(figsize=(10, 10))
 plt.subplot(211)
-plt.plot(tempo, r, '-b', linewidth=1.2)
+plt.plot(tempo, r + 4.56, '-b', tempo, y , '-ro', linewidth=1.2)
 plt.xlabel('Tempo(s)')
 plt.ylabel('Tensão (V)')
 plt.grid()
 plt.title('Onda Quadrada - Malha Aberta')
-plt.legend(loc='lower right', labels=('Sinal de Entrada', 'Sinal de Saída'))
+plt.legend(loc='lower right', labels=('Sinal de Entrada','Sinal de Saída'))
 
 plt.subplot(212)
 # plt.plot(tempo,r,'-b',tempo,y,'-r',linewidth=1.2)
-plt.plot(tempo, y, '-ro', linewidth=1.2)
+plt.plot(tempo, u, '-ko', linewidth=1.2)
 plt.xlabel('Tempo(s)')
 plt.ylabel('Tensão (V)')
 plt.grid()
@@ -89,4 +100,5 @@ plt.show()
 
 dados = np.stack((tempo, r, y), axis=-1)
 
-# np.savetxt("6_5_dados_motorgerador.csv", dados, delimiter=";")
+np.savetxt("malha_fechada_40pc_dados_motorgerador.csv", dados, delimiter=";")
+# np.save(r"C:\Users\Windows 10\OneDrive\Área de Trabalho\Victor\ensaio_sen02", dados)
